1a80cee41d37e21560abe9ef534890736c57c50f
asftian <asftian@gmail.com>
2015-09-28 15:31:45 +0100

    add .gitignore

--- Roll-a-Ball/Assets/Scripts/PlayerController.cs
+++ Roll-a-Ball/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
@@ -6,14 +7,30 @@ public class PlayerController : MonoBehaviour {
 	public float speed;
 	private Rigidbody rb;
 
+	private int count;
+	private float playTime;
+	private bool stopTiming;
+
+	public Text countText;
+	public Text winText;
+	public Text playTimeText;
+
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody> (); 
+		count = 0;
+		playTime = 0.0f;
+		stopTiming = false;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		playTime = playTime + Time.deltaTime;
+		if (!stopTiming )
+			playTimeText.text = "Play time: " + Mathf.RoundToInt (playTime ).ToString ();
 	}
 
 	void FixedUpdate()
@@ -24,4 +41,29 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVerticial);
 		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText();
+
+			//Debug.Log ("count = " + count.ToString());
+		
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Count: " + count.ToString ();
+		playTimeText.text = "Play time: " + Mathf.RoundToInt (playTime ).ToString ();
+		if (count == 10) {
+			stopTiming = true;
+			winText.text = "You Win!";
+			Debug.Log ("all objects are collected!");
+		}
+	}
+	
 }
