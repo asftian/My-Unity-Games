00b6f8c78fd35b453490731bfcc811ce50695289
asftian <ftian@PC30688.staff.bournemouth.ac.uk>
2015-09-29 16:30:26 +0100

    testing scripts by adding some objects

---
+++ Roll-a-Ball/Assets/capsularRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class capsularRotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (1, 2, 3));
+
+	}
+}
---
+++ Roll-a-Ball/Assets/myTestScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myTestScript : MonoBehaviour {
+
+	int speed = 5;
+
+	public int size = 100;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log (speed);
+		Debug.Log (size);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
