c117be6c9de47feff3542e708a7b91b10e9fe9e7
asftian <asftian@gmail.com>
2015-09-29 23:39:45 +0100

    a new project for the lab 2 unity scripting

---
+++ Lab-2-Scripting/Assets/Scripts/CubeController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeController : MonoBehaviour {
+
+    private float speed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // move the object by 1 meter per frame
+        //transform.Translate(1, 0, 0);
+
+        // move the object by 1 meter per second
+        //transform.Translate(1 * Time.deltaTime, 0, 0);
+
+        // rotate the cube
+        //transform.Rotate(45, 45, 45);
+        //transform.Rotate(45 * Time.deltaTime, 45 * Time.deltaTime, 45 * Time.deltaTime);
+
+        ControlByArrowKeys();
+        ControlByArrowKeys(speed);
+	
+	}
+
+    void ControlByArrowKeys()
+    {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        transform.Translate(xMove, 0.0f, zMove);
+    }
+
+    void ControlByArrowKeys(float speed)
+    {
+        float xMove = Input.GetAxis("Horizontal") * speed;
+        float zMove = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(xMove, 0.0f, zMove);
+    }
+}
