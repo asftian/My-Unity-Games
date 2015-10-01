f39117eef75bf4a42acf3167792197cd65e9fcf3
asftian <ftian@PC30688.staff.bournemouth.ac.uk>
2015-10-01 16:53:42 +0100

    add a script for the new object 'Sphere'
    inter object communication through variables.

---
+++ Lab-2-Scripting/Assets/Scripts/SphereController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereController : MonoBehaviour {
+
+    public float speed;
+
+    public GameObject myCube;
+
+	// Use this for initialization
+	void Start () {
+        speed = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //transform.Translate(1 * Time.deltaTime * speed, 0.0f, 3 * Time.deltaTime * speed);
+        //myCube.transform.Translate(1 * Time.deltaTime * speed, 0.0f, 3 * Time.deltaTime * speed);
+        ControlByArrowKeys(speed);
+	}
+
+    void ControlByArrowKeys(float speed)
+    {
+        float xMove = Input.GetAxis("Horizontal") * speed;
+        float zMove = Input.GetAxis("Vertical") * speed;
+
+        transform.Translate(xMove, 0.0f, zMove);
+        myCube.transform.Translate(xMove, 0.0f, zMove);
+    }
+}
