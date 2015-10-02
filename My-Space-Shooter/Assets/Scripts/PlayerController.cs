7e15ba2057fc86d15b2bdbcd0e307427133367c6
asftian <asftian@gmail.com>
2015-10-02 23:07:46 +0100

    spaw and shoot shots

--- My-Space-Shooter/Assets/Scripts/PlayerController.cs
+++ My-Space-Shooter/Assets/Scripts/PlayerController.cs
@@ -15,9 +15,26 @@ public class PlayerController : MonoBehaviour {
 
 	private Rigidbody rb;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	public float fireRate;
+	private float nextFire;
+
 	void Start()
 	{
 		rb = GetComponent <Rigidbody > ();
+	}
+
+	void Update()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) // 'Fire 1' is the left mouse button
+		{
+			nextFire = Time.time + fireRate;
+			// GameObject clone = 
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation); // as GameObject ;
+		}
+
 	}
 	
 	void FixedUpdate()
@@ -25,7 +42,7 @@ public class PlayerController : MonoBehaviour {
 
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
-			
+	
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rb.velocity = movement * speed;
 
