2a327a26a665a6fd9371202aaaaf06d02ec68b17
asftian <asftian@gmail.com>
2015-10-05 23:19:40 +0100

    Added 'Audio' support and scoring text

--- My-Space-Shooter/Assets/Scripts/GameController.cs
+++ My-Space-Shooter/Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using UnityEngine.UI;
+
 public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
@@ -9,9 +11,14 @@ public class GameController : MonoBehaviour {
 	public float spawnWait;
 	public float startWait;
 	public float waveWait;
+
+	public Text scoreText;
+	private int score;
 	
 	void Start ()
 	{
+		score = 0;
+		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 	}
 	
@@ -29,5 +36,16 @@ public class GameController : MonoBehaviour {
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
+	}
+
+	public void AddScore (int newScoreValue)
+	{
+		score += newScoreValue;
+		UpdateScore ();
+	}
+	
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: " + score.ToString ();
 	}
 }
