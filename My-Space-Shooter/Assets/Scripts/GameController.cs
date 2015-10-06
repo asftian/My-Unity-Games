b1df8c5238fba4da6f20db216658243611be1eed
asftian <asftian@gmail.com>
2015-10-06 21:50:44 +0100

    Ending the game
    add game over text and restart with the key 'R'.

--- My-Space-Shooter/Assets/Scripts/GameController.cs
+++ My-Space-Shooter/Assets/Scripts/GameController.cs
@@ -14,14 +14,39 @@ public class GameController : MonoBehaviour {
 
 	public Text scoreText;
 	private int score;
-	
+
+	public Text gameOverText;
+	public Text restartText;
+
+	private bool gameOver;
+	private bool restart;
+
 	void Start ()
 	{
+
+		gameOverText.text = "";
+		restartText.text = "";
+		gameOver = false;
+		restart = false;
+
 		score = 0;
 		UpdateScore ();
 		StartCoroutine (SpawnWaves ());
 	}
-	
+
+	public void Update()
+	{
+		if (restart) 
+		{
+			if(Input.GetKeyDown(KeyCode.R))
+			{
+				Application .LoadLevel (Application.loadedLevel );
+			}
+
+		}
+	}
+
+
 	IEnumerator SpawnWaves ()
 	{
 		yield return new WaitForSeconds (startWait);
@@ -34,8 +59,25 @@ public class GameController : MonoBehaviour {
 				Instantiate (hazard, spawnPosition, spawnRotation);
 				yield return new WaitForSeconds (spawnWait);
 			}
+
+			
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
+
 			yield return new WaitForSeconds (waveWait);
 		}
+	}
+
+	//tian
+	public void GameOver()
+	{
+		gameOverText.text = "Game Over!";
+		gameOver  = true;
+
 	}
 
 	public void AddScore (int newScoreValue)
