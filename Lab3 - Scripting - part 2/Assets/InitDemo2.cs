96c12c26e01a1484269444dd2133da041530b7f7
asftian <asftian@gmail.com>
2015-10-18 18:35:51 +0100

    new tutorial from Unity - survival shooter

---
+++ Lab3 - Scripting - part 2/Assets/InitDemo2.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitDemo2 : MonoBehaviour {
+
+	void Awake()
+	{
+		//Debug.Log ("Cube 2 - Awake");
+	}
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("Cube 2 - Start");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Lab3 - Scripting - part 2/Assets/InitializationDemo.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitializationDemo : MonoBehaviour {
+
+
+	void Awake()
+	{
+		//Debug.Log ("Cube 1 - Awake");
+	}
+	// Use this for initialization
+	void Start () {
+		//Debug.Log ("Cube 1 - Start");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug .Log ("Cube 1 - Update - " + Time.deltaTime );
+	}
+
+	void FixedUpdate() {
+		Debug .Log ("Cube 1 - Fixed update - " + Time.deltaTime );
+	}
+
+} 
